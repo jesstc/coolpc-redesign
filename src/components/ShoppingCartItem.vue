141c9e26c013fd5b396f5866da9498f84cf47d6e
Jing Han, Huang <7jesstc1999@gmail.com>
2024-08-12 14:08:42 +0800

    feat: add ShoppingCartItem component

---
+++ src/components/ShoppingCartItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <n-flex justify="space-between" class="w-full h-24 overflow-hidden items-center">
+    <img v-if="props.isPage" class="w-3/12 h-24 object-scale-down bg-slate-300"
+      src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
+
+    <n-space vertical class="w-3/12">
+      <h3>產品名稱</h3>
+      <p v-if="props.isPage">產品規格：產品規格產品規格產品規格產品規格產品規格產品規格產品規格產品規格產品規格產品規格</p>
+    </n-space>
+
+    <p>$單價</p>
+
+    <!-- number btn group -->
+    <n-input-number class="w-1/12 h-fit text-center" 
+      button-placement="both"
+      :default-value="0"
+      min="0" max="100"
+    />
+
+    <p v-if="props.isPage">$總價</p>
+    
+    <!-- add to cart btn -->
+    <n-button strong secondary type="error" class="h-fit">
+      <template #icon>
+        <n-icon><TrashOutline /></n-icon>
+      </template>
+      <span v-if="props.isPage">刪除</span>
+    </n-button>
+  </n-flex>
+</template>
+
+<script setup lang="ts">
+import { NIcon } from 'naive-ui'
+import { TrashOutline } from '@vicons/ionicons5'
+import { defineProps } from 'vue';
+
+export interface Props {
+  isPage?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isPage: true,
+})
+
+</script>
--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>
-      <h1>購物車</h1>
-      <p>這是購物車</p>
-    </div>
-  </template>
+  <n-flex vertical :size="10">
+    <h1>購物車</h1>
+
+    <n-flex vertical>
+      <h3>選購產品</h3>
+      <ShoppingCartItem />
+    </n-flex>
+
+    <n-flex vertical>
+      <h3>付款聲明</h3>
+    </n-flex>
+  </n-flex>
+</template>
   
 <script setup lang="ts">
+import ShoppingCartItem from '../components/ShoppingCartItem.vue'
 </script>
