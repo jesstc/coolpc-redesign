fa0c0269906e75499d38ce1acc61ba82cfc7b063
Jing Han, Huang <7jesstc1999@gmail.com>
2024-08-13 13:58:38 +0800

    style: change the shopping cart items style

--- src/components/ShoppingCartItem.vue
+++ src/components/ShoppingCartItem.vue
@@ -1,23 +1,28 @@
 <template>
-  <n-flex justify="space-between" class="w-full h-24 overflow-hidden items-center">
-    <img v-if="props.isPage" class="w-3/12 h-full object-scale-down bg-slate-300 items-center"
+  <n-flex justify="space-around"
+    class="w-full h-fit py-4 overflow-hidden items-center"
+    :style="{ backgroundColor: themeVars.tableColorHover }">
+    
+    <img v-if="props.isPage" class="w-3/12 h-24 object-scale-down bg-slate-300 items-center"
       src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
 
-    <n-flex vertical class="w-3/12">
+    <n-flex vertical :size="8" class="w-3/12">
       <h3>產品名稱</h3>
-      <p v-if="props.isPage">產品規格：產品規格產品規格產品規格產品規格產品規格產品規格產品規格產品規格產品規格產品規格</p>
+      <span v-if="props.isPage" :style="{ color: themeVars.clearColorHover }">
+        產品規格：產品規格產品規格產品規格產品規格產品規格
+      </span>
     </n-flex>
 
-    <p>$單價</p>
+    <p>$10000</p>
 
     <!-- number btn group -->
     <n-input-number class="w-1/12 h-fit text-center" 
       button-placement="both"
-      :default-value="0"
-      min="0" max="100"
+      :default-value="2"
+      min="1" max="100"
     />
 
-    <p v-if="props.isPage">$總價</p>
+    <p v-if="props.isPage">$20000</p>
     
     <!-- add to cart btn -->
     <n-button strong secondary type="error" class="h-fit">
@@ -30,9 +35,11 @@
 </template>
 
 <script setup lang="ts">
-import { NFlex, NButton, NIcon, NInputNumber } from 'naive-ui'
+import { NFlex, NButton, NIcon, NInputNumber, useThemeVars } from 'naive-ui'
 import { TrashOutline } from '@vicons/ionicons5'
 import { defineProps } from 'vue';
+
+const themeVars = useThemeVars();
 
 export interface Props {
   isPage?: boolean;
