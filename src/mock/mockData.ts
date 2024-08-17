88500a26ecddab7c37a2d857a959799f203aa276
Jing Han, Huang <7jesstc1999@gmail.com>
2024-08-17 16:38:31 +0800

    fix: can generate different categories of product data

--- src/mock/mockData.ts
+++ src/mock/mockData.ts
@@ -13,16 +13,17 @@ export const categoriesWithBrands: Record<string, string[]> = categories.reduce(
 }, {} as Record<string, string[]>);
 
 export function generateMockProducts(count: number): ProductInfo[] {
-  const category = faker.helpers.arrayElement(categories);
-
-  return Array.from({ length: count }).map((_cur, index) => ({
-    id: index,
-    name: faker.commerce.productName(),
-    imgUrl: faker.image.urlLoremFlickr() ,
-    price: parseInt(faker.commerce.price({ min: 1000, max: 50000, dec: 0 })),
-    brand: faker.helpers.arrayElement(categoriesWithBrands[category]),  // select from 'categoriesWithBrands'
-    category,
-    description: faker.commerce.productDescription(),
-    openboxUrl: Math.random() > 0.5 ? faker.internet.url() : undefined, // 50% 的機率會有
-  }));
+  return Array.from({ length: count }).map((_cur, index) => {
+    const category = faker.helpers.arrayElement(categories);
+    return {
+      id: index,
+      name: faker.commerce.productName(),
+      imgUrl: faker.image.urlLoremFlickr() ,
+      price: parseInt(faker.commerce.price({ min: 1000, max: 50000, dec: 0 })),
+      brand: faker.helpers.arrayElement(categoriesWithBrands[category]),  // select from 'categoriesWithBrands'
+      category,
+      description: faker.commerce.productDescription(),
+      openboxUrl: Math.random() > 0.5 ? faker.internet.url() : undefined, // 50% 的機率會有
+    };
+  });
 }
