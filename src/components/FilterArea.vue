d12f4ba183e80c713a04d3fde742e300b534cf8d
Jing Han, Huang <7jesstc1999@gmail.com>
2024-08-17 17:14:36 +0800

    fix: make sure the price range follows [min, max]

--- src/components/FilterArea.vue
+++ src/components/FilterArea.vue
@@ -18,10 +18,10 @@
                   :marks="marks" :step="100" :min="500" :max="50000" />
         <div class="flex justify-between items-center w-full">
           <n-input-number class="w-5/12 inline text-center" 
-                          v-model:value="priceRange[0]" size="small" :show-button="false" :step="100"/>
+                          v-model:value="priceRange[0]" size="small" :show-button="false" :step="100" :min="500" :max="priceRange[1]"/>
           <span>～</span>
           <n-input-number class="w-5/12 inline text-center" 
-                          v-model:value="priceRange[1]" size="small" :show-button="false" :step="100"/>
+                          v-model:value="priceRange[1]" size="small" :show-button="false" :step="100" :min="priceRange[0]" :max="50000"/>
         </div>
       </n-space>
 
@@ -106,6 +106,10 @@ const handleUpdateBrands = (value: string):void => {
 const priceRange = ref(filters.value.priceRange);
 const marks = ref({ [priceRange.value[0]]: 'min', [priceRange.value[1]]: 'max' });
 watch(priceRange, (newVal) => {
+  if (priceRange.value[0] > priceRange.value[1]) {
+    // switch the numbers to make sure [min, max] (and min < max)
+    [priceRange.value[0], priceRange.value[1]] = [priceRange.value[1], priceRange.value[0]];
+  }
   productStore.updateFilters(undefined, undefined, newVal);
 }, { deep: true })
 
