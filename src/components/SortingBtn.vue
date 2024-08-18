9d6aef9303f0b2188c298a199b4455231f0afea7
Jing Han, Huang <7jesstc1999@gmail.com>
2024-08-18 14:37:56 +0800

    refactor: display the sorting btn status based on the product soter state

--- src/components/SortingBtn.vue
+++ src/components/SortingBtn.vue
@@ -2,28 +2,22 @@
   <n-flex class="items-center">
     <h4>排序依據：</h4>
     <n-button-group>
-      <n-button icon-placement="right"  @click="categorySorting">
+      <n-button icon-placement="right"  @click="sorting('category')">
         產品類別
         <template #icon>
-          <n-icon v-if="categorySort%2 == 1" :component="ArrowSortUp20Regular" />
-          <n-icon v-else-if="categorySort%2 == 0" :component="ArrowSortDown20Regular" />
-          <n-icon v-else :component="ArrowSort20Regular" />
+          <n-icon :component="getSortBtn('category')" />
         </template>
       </n-button>
-      <n-button icon-placement="right" @click="priceSorting">
+      <n-button icon-placement="right" @click="sorting('price')">
         價格
         <template #icon>
-          <n-icon v-if="priceSort%2 == 1" :component="ArrowSortUp20Regular" />
-          <n-icon v-else-if="priceSort%2 == 0" :component="ArrowSortDown20Regular" />
-          <n-icon v-else :component="ArrowSort20Regular" />
+          <n-icon :component="getSortBtn('price')" />
         </template>
       </n-button>
-      <n-button icon-placement="right" @click="brandSorting">
+      <n-button icon-placement="right" @click="sorting('brand')">
         品牌
         <template #icon>
-          <n-icon v-if="brandSort%2 == 1" :component="ArrowSortUp20Regular" />
-          <n-icon v-else-if="brandSort%2 == 0" :component="ArrowSortDown20Regular" />
-          <n-icon v-else :component="ArrowSort20Regular" />
+          <n-icon :component="getSortBtn('brand')" />
         </template>
       </n-button>
     </n-button-group>
@@ -31,48 +25,25 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import { ArrowSort20Regular, ArrowSortDown20Regular, ArrowSortUp20Regular } from '@vicons/fluent'
 import { NFlex, NButtonGroup, NButton, NIcon } from 'naive-ui'
 import { useProductStore } from '../stores/product';
+import { storeToRefs } from "pinia";
 
 const productStore = useProductStore();
+const { sorter } = storeToRefs(productStore);
 
-const categorySort = ref<number>(-1);
-const priceSort = ref<number>(-1);
-const brandSort = ref<number>(-1);
-
-const resetSortingStatus = (currentSort: "price" | "brand" | "category"):void => {
-  let isAsc:boolean = false;
-
-  if (currentSort != 'category') categorySort.value = -1;
-  else {
-    categorySort.value = (categorySort.value+1) % 2;
-    categorySort.value == 0 && (isAsc = true);
-  }
-  if (currentSort != 'price') priceSort.value = -1;
-  else {
-    priceSort.value = (priceSort.value+1) % 2;
-    priceSort.value == 0 && (isAsc = true);
-  }
-  if (currentSort != 'brand') brandSort.value = -1;
-  else {
-    brandSort.value = (brandSort.value+1) % 2;
-    brandSort.value == 0 && (isAsc = true);
-  }
-
-  productStore.updateSorter(currentSort, isAsc);
+// update the sorting btn status
+const sorting = (sortType: 'category' | 'price' | 'brand'):void => {
+  let isAsc:boolean = sorter.value.base == sortType ? !sorter.value.isAsc : true;
+  productStore.updateSorter(sortType, isAsc);
+  console.log(sorter.value)
 }
 
-const categorySorting = ():void => {
-  resetSortingStatus('category');
-}
-
-const priceSorting = ():void => {
-  resetSortingStatus('price');
-}
-
-const brandSorting = ():void => {
-    resetSortingStatus('brand');
+// decide the display btn
+const getSortBtn = (sortType: 'category' | 'price' | 'brand') => {
+  if (sorter.value.base !== sortType) return ArrowSort20Regular;
+  else if (sorter.value.isAsc) return ArrowSortDown20Regular;
+  else return ArrowSortUp20Regular;
 }
 </script>
