e05ae3e099db3d4985b8578e996ce9fc643a3bfa
Jing Han, Huang <7jesstc1999@gmail.com>
2024-08-20 19:14:07 +0800

    style: add filterArea rwd

--- src/components/FilterArea.vue
+++ src/components/FilterArea.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-card embedded :bordered="false" class="flex w-full gap-5">
+  <n-card v-if="props.isDesktop" embedded :bordered="false" class="flex w-full gap-5">
     <template #header>
       <n-flex class="flex items-center">
         <n-icon :component="FunnelOutline" :color="themeVars.primaryColorPressed" /> 
@@ -45,16 +45,87 @@
 
     </n-flex>
   </n-card>
+
+  <!-- filter btn (show when not desktop) -->
+  <n-button v-else @click="showModal = true">
+    <template #icon>
+      <n-icon :component="FunnelOutline" />
+    </template>
+    篩選
+  </n-button>
+  <n-modal v-model:show="showModal">
+    <n-card 
+      :bordered="false" aria-modal="true" closable
+      :style="isMobile ? 'width: 100vw; position: fixed; bottom: 0px;' : 'width: 50vw;'" 
+    >
+      <template #header>
+        <n-flex class="flex items-center">
+          <n-icon :component="FunnelOutline" :color="themeVars.primaryColorPressed" /> 
+          <h1 :style="{ color: themeVars.primaryColorPressed }">篩選條件</h1>
+        </n-flex>
+      </template>
+
+      <template #default>
+        <n-flex vertical wrap-item :size="32">
+
+          <n-flex vertical>
+            <h3>產品類別</h3>
+            <n-select
+              placeholder="請選擇產品類別"
+              :options="filterOptions.categories"
+              :value="filters.category"
+              @update:value="handleUpdateCategories"
+            />
+          </n-flex>
+          
+          <n-flex vertical :size="18">
+            <h3>價格區間</h3>
+            <n-slider v-model:value="priceRange" range
+                      :marks="marks" :step="100" :min="500" :max="50000" />
+            <div class="flex justify-between items-center w-full">
+              <n-input-number class="w-5/12 inline text-center" 
+                              v-model:value="priceRange[0]" size="small" :show-button="false" :step="100" :min="500" :max="priceRange[1]"/>
+              <span>～</span>
+              <n-input-number class="w-5/12 inline text-center" 
+                              v-model:value="priceRange[1]" size="small" :show-button="false" :step="100" :min="priceRange[0]" :max="50000"/>
+            </div>
+          </n-flex>
+
+          <n-flex vertical>
+            <h3>品牌</h3>
+            <n-select
+              multiple filterable
+              :placeholder="filterOptions.brands.length ? '請選擇品牌' : '請先選擇產品類別'"
+              :options="filterOptions.brands"
+              v-model:value="filters.brand"
+              @update:value="handleUpdateBrands"
+            />
+          </n-flex>
+
+        </n-flex>
+      </template>
+    </n-card>
+  </n-modal>
 </template>
 
 <script setup lang="ts">
-import { NCard, NFlex, NInputNumber, NSelect, NSlider, NIcon, useThemeVars } from 'naive-ui'
-import { ref, onMounted, watch } from 'vue'
+import { NCard, NModal, NFlex, NInputNumber, NSelect, NSlider, NIcon, NButton, useThemeVars } from 'naive-ui'
+import { useWindowSize } from '@vueuse/core';
+import { ref, onMounted, watch, computed } from 'vue'
 import { FunnelOutline } from '@vicons/ionicons5';
 import { storeToRefs } from "pinia";
 import { useProductStore } from '../stores/product';
 
+interface Props {
+  isDesktop: Boolean,
+}
+const props = defineProps<Props>();
+
 const themeVars = useThemeVars();
+
+const showModal = ref(false);
+const { width } = useWindowSize();
+const isMobile = computed(() => width.value < 768);
 
 const productStore = useProductStore();
 const { filters, filterOptions } = storeToRefs(productStore);
